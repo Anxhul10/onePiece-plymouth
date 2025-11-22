043d8e5586403723ff5ad1d2cc3fde5cae7a54eb
Anxhul10 <anxhul10@gmail.com>
2025-11-22 16:27:43 +0530

    chore: add fast and slow flag

--- cli/init.js
+++ cli/init.js
@@ -1,5 +1,28 @@
 import chalk from 'chalk';
 import boxen from 'boxen';
+import { Command } from 'commander';
+
+const program = new Command();
 
 
-console.log(boxen(chalk.red('welcome to onePiece-plymouth'), {padding: 1, margin: 1, borderStyle: 'double'}));
+program
+  .name('onePiece-plymouth')
+  .description('CLI to install onePiece-plymouth')
+  .version('0.0.');
+
+program
+  .option('--fast', 'fast animation')
+  .option('--slow', 'slow animation')
+
+program.parse();
+
+if (program.opts().fast) {
+    console.log('install fast animation')
+}
+else if(program.opts().slow) {
+    console.log('install slow animation')
+}
+else {
+    console.log('run npx onePiece-plymouth --help')
+}
+
