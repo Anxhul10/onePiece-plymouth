939b0f64fdbfd560fb04181838b1725196f88484
Anxhul10 <anxhul10@gmail.com>
2025-11-25 17:24:09 +0530

    fix: add i/p wait

--- main.sh
+++ main.sh
@@ -13,7 +13,7 @@ if [ osCheck $1 ]; then
     if [[ "$(id -u)" -ne 0 ]]; then
         echo "Please run this as root"
     else 
-        read -p "Enter the priority of plymouth : " priority
+        read -p "Enter the priority of plymouth : " priority < /dev/tty
         cd  /usr/share/plymouth/themes
         sudo git clone https://github.com/Anxhul10/onePiece-plymouth.git
         sudo update-alternatives --install /usr/share/plymouth/themes/default.plymouth default.plymouth /usr/share/plymouth/themes/onePiece-plymouth/onePiece-plymouth.plymouth $priority
@@ -25,7 +25,7 @@ if [ osCheck $1 ]; then
         echo "choose animation speed:"
         echo "1. faster animation"
         echo "2. slower animation"
-        read -n 1 -p "Enter the choice(eg. 1 or 2) : " choice
+        read -n 1 -p "Enter the choice(eg. 1 or 2) : " choice < /dev/tty
 
         if [[ $choice == 1 ]]; then
             printf "\nfast animation enabled !!"
