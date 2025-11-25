3b68183cd921c89c7395c9ff87a235ea1977f8bd
Anxhul10 <anxhul10@gmail.com>
2025-11-25 14:31:14 +0530

    chore: add option fast and slow animation

--- main.sh
+++ main.sh
@@ -13,12 +13,25 @@ if [ osCheck $1 ]; then
     if [ "$(id -u)" -ne 0 ]; then
         echo "Please run this as root"
     else 
+        read -p "Enter the priority of plymouth : " priority
         cd  /usr/share/plymouth/themes
         sudo git clone https://github.com/Anxhul10/onePiece-plymouth.git
-        sudo update-alternatives --install /usr/share/plymouth/themes/default.plymouth default.plymouth /usr/share/plymouth/themes/onePiece-plymouth/onePiece-plymouth.plymouth 250
+        sudo update-alternatives --install /usr/share/plymouth/themes/default.plymouth default.plymouth /usr/share/plymouth/themes/onePiece-plymouth/onePiece-plymouth.plymouth $priority
         sudo update-alternatives --config default.plymouth
         sudo update-initramfs -u
         # ask user for fast and slow animation
+        echo "###############################################"
+        echo "choose animation speed:"
+        echo "1. faster animation"
+        echo "2. slower animation"
+        read -n 1 -p "Enter the choice(eg. 1 or 2) : " choice
+        echo "$choice hahaha"
+
+        if [ $choice eq 2 ]; then
+            echo "edit the counnt"
+        else 
+            echo "slower animation saved !!"
+        fi
     fi
 else 
     echo "Please use linux-gnu"
