6ca1b250bfd1354a16e32cbf04ddf56c81775a13
Anxhul10 <anxhul10@gmail.com>
2025-11-25 17:06:39 +0530

    chore: add root condition

--- main.sh
+++ main.sh
@@ -10,27 +10,31 @@ osCheck() {
 }
 
 if [ osCheck $1 ]; then
-    read -p "Enter the priority of plymouth : " priority
-    cd  /usr/share/plymouth/themes
-    sudo git clone https://github.com/Anxhul10/onePiece-plymouth.git
-    sudo update-alternatives --install /usr/share/plymouth/themes/default.plymouth default.plymouth /usr/share/plymouth/themes/onePiece-plymouth/onePiece-plymouth.plymouth $priority
-    sudo update-alternatives --config default.plymouth
-    sudo update-initramfs -u
-    cd onePiece-plymouth/
-    # ask user for fast and slow animation
-    echo "###############################################"
-    echo "choose animation speed:"
-    echo "1. faster animation"
-    echo "2. slower animation"
-    read -n 1 -p "Enter the choice(eg. 1 or 2) : " choice
+    if [ "$(id -u)" -ne 0 ]; then
+        echo "Please run this as root"
+    else 
+        read -p "Enter the priority of plymouth : " priority
+        cd  /usr/share/plymouth/themes
+        sudo git clone https://github.com/Anxhul10/onePiece-plymouth.git
+        sudo update-alternatives --install /usr/share/plymouth/themes/default.plymouth default.plymouth /usr/share/plymouth/themes/onePiece-plymouth/onePiece-plymouth.plymouth $priority
+        sudo update-alternatives --config default.plymouth
+        sudo update-initramfs -u
+        cd onePiece-plymouth/
+        # ask user for fast and slow animation
+        echo "###############################################"
+        echo "choose animation speed:"
+        echo "1. faster animation"
+        echo "2. slower animation"
+        read -n 1 -p "Enter the choice(eg. 1 or 2) : " choice
 
-    if [ $choice == 1 ]; then
-        echo "fast animation enabled !!"
-    fi
-    if [ $choice == 2 ]; then
-        ls
-        cp onePiece-plymouth-slow.script onePiece-plymouth.script
-        echo "slow animation enabled !!"
+        if [ $choice == 1 ]; then
+            echo "fast animation enabled !!"
+        fi
+        if [ $choice == 2 ]; then
+            ls
+            cp onePiece-plymouth-slow.script onePiece-plymouth.script
+            echo "slow animation enabled !!"
+        fi
     fi
 else 
     echo "Please use linux-gnu"
