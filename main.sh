c733da31007fe1ec0d2e2a2d43cc29031d10d01b
Anxhul10 <anxhul10@gmail.com>
2025-11-25 14:08:07 +0530

    chore: add root and oschecks

--- main.sh
+++ main.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 
-if [ "$(id -u)" -ne 0 ]; then
-    echo "please run as root"
+osCheck() {
+    if [ $OSTYPE == "linux-gnu" ]; then
+        echo "1 from "
+        return 1
+    fi
+    echo "0 last"
+    return 0
+}
+
+if [ osCheck $1 ]; then
+    if [ "$(id -u)" -ne 0 ]; then
+        echo "not root"
+    else 
+        echo "root"
+    fi
 else 
-    echo "running as root"
+    echo "Please use linux-gnu"
 fi
+
